d0b1f3352ac1750270cce1e587b8b43fab2da57d
Techwiz2209 <ugo_christopher2000@yahoo.com>
2023-03-16 21:55:00 -0700

    update again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include<studio>.h>
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Alware 0 (success)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
